8c507af46d9ab4fe12192d33cc62d3f5873a75e6
Jayzkie Beats <jaygolbin24@gmail.com>
2026-09-03 00:06:22 +0800

    Updated the system based on the Composer: 2.10.3 and PHP: 8.2.12 version

--- config/database.php
+++ config/database.php
@@ -19,7 +19,7 @@ $baseOptions = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
-    Pdo\Mysql::ATTR_INIT_COMMAND => $strictMode,
+    PDO::MYSQL_ATTR_INIT_COMMAND => $strictMode,
 ];
 
 $configs = [
